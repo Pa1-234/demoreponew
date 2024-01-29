162d07e2cb67f13903b8cb67079cc588cde5bc07
nikshithas291993 <nikshitha.s@sonata-software.com>
2024-01-29 11:49:32 +0530

    Initial Commit

---
+++ service/microone/src/main/java/com/mydomain/MicrooneApplication.java
@@ -0,0 +1,13 @@
+package com.mydomain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrooneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrooneApplication.class, args);
+	}
+
+}
---
+++ service/microone/src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO product (product_id,currency,list_price,product_image,product_name,product_stock,sales_price) VALUES ('p0001','INR',130.0,'https://assets.ajio.com/medias/sys_master/root/20230519/hpj9/64667a8142f9e729d7996b8a/-473Wx593H-466165120-wine-MODEL8.jpg','product1','20',180.0);
+INSERT INTO product (product_id,currency,list_price,product_image,product_name,product_stock,sales_price) VALUES ('p0002','INR',350.0,'https://assets.ajio.com/medias/sys_master/root/20230519/hpj9/64667a8142f9e729d7996b8a/-473Wx593H-466165120-wine-MODEL8.jpg','product2','28',430.0);
